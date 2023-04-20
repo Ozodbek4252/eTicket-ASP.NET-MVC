2db8212091998d644135ad7bebb398454a387b9f
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-20 12:06:38 +0500

    configure ActorService and use it

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,6 +1,13 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
 {
     public interface IActorsService
     {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
     }
 }
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
+builder.Services.AddScoped<IActorsService, ActorsService>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +21,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 
 using (var scope = app.Services.CreateScope())

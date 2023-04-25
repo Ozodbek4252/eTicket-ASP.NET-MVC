c093f3adddb779194b3d4163c7377b67345e950a
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-25 11:59:42 +0500

    Actor edit and delete

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -3,6 +3,13 @@ using eTickets.Models;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 
+//using eTickets.Data.Static;
+//using Microsoft.AspNetCore.Authorization;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
 namespace eTickets.Controllers
 {
     public class ActorsController : Controller
@@ -27,13 +34,12 @@ namespace eTickets.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Actor actor)
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")]Actor actor)
         {
             if (!ModelState.IsValid)
             {
                 return View(actor);
             }
-
             await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
         }
@@ -43,8 +49,49 @@ namespace eTickets.Controllers
         {
             var actorDetailts = await _service.GetByIdAsync(id);
 
-            if (actorDetailts == null) return View("Empty");
+            if (actorDetailts == null) return View("NotFound");
             return View(actorDetailts);
+        }
+
+        // Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetailts = await _service.GetByIdAsync(id);
+
+            if (actorDetailts == null) return View("NotFound");
+            return View(actorDetailts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfilePictureURL,FullName,Bio")]Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetailts = await _service.GetByIdAsync(id);
+
+            if (actorDetailts == null) return View("NotFound");
+            return View(actorDetailts);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetailts = await _service.GetByIdAsync(id);
+
+            if (actorDetailts == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+//using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace eTickets.Models
 {
@@ -8,19 +14,19 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
-        [Required(ErrorMessage = "Profile picture is required")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
-        
+
         [Display(Name = "Full Name")]
-        [Required(ErrorMessage = "Full Name picture is required")]
+        [Required(ErrorMessage = "Full Name is required")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
-        public string FullName { get; set;}
+        public string FullName { get; set; }
 
-        [Display(Name = "Bio")]
-        [Required(ErrorMessage = "Bio is required")]
-        public string Bio { get; set;}
+        [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+        public string Bio { get; set; }
 
-        // Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        //Relationships
+        public List<Actor_Movie> ?Actors_Movies { get; set; }
     }
 }
